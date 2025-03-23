3c6b9fcc42ae144c331c62c278d5d4fbe7bb44ca
mingri31164 <3116430062@qq.com>
2025-03-23 22:24:38 +0800

    优化项目初次调用响应

---
+++ frontend/commons/src/main/java/com/mingri/prize/commons/constant/WebConstant.java
@@ -0,0 +1,11 @@
+package com.mingri.prize.commons.constant;
+
+/**
+ * web服务常量信息
+ */
+public class WebConstant {
+
+    public final static String INITIALIZE_PATH = "/initialize/dispatcher-servlet";
+
+
+}
